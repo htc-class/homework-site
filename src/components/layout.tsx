60de493b3a7e40f42e7fbc70ccf933f8329c41bb
Jared Henderson <jaredh159@users.noreply.github.com>
2022-11-07 10:39:03 -0500

    add link to gertrude.app for seo

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,6 +7,12 @@ const Layout = ({ children }) => {
       <div className="p-10 max-w-3xl">
         <main>{children}</main>
       </div>
+      <footer className="text-center text-gray-500 text-sm p-4">
+        © {new Date().getFullYear()} | Check out{" "}
+        <a href="https://gertrude.app">
+          Gertrude, a macOS internet filter app.
+        </a>
+      </footer>
     </div>
   );
 };
